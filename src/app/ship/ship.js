3e2515485cafb55960f4f0408f5df24d6ad6a57d
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-10 12:55:59 -0600

    feat: implement hit() method

--- src/app/ship/ship.js
+++ src/app/ship/ship.js
@@ -1,7 +1,11 @@
-class Ship  {
+export class Ship  {
     constructor(length, hits = 0, id) {
         this.length = length,
         this.hits = hits,
         this.id = id;
+    }
+
+    hit() {
+        this.hits++;
     }
 }
