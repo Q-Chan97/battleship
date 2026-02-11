ec0ea77450fd5e2f3e80ab7d996fc870ab0c601b
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-11 13:10:43 -0600

    feat: Create board array

--- src/app/gameboard/gameboard.js
+++ src/app/gameboard/gameboard.js
@@ -1,7 +1,12 @@
 export class GameBoard {
     constructor(rows = 10, cols = 10) {
-        this.rows = rows,
-        this.cols = cols
+        this.rows = rows;
+        this.cols = cols;
+        
+        // Create board, rows x columns, cells initialized to null
+        this.board = Array.from({ length: rows }, () =>  
+            Array(cols).fill(null)
+        );
 
         this.ships = []; // Keep track of ships
         this.missedShots = []; // Keep track of coords of missed shots
