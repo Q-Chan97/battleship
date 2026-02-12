109dc19a13754f317729248bbd34bf28066f2336
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-12 12:23:13 -0600

    feat: Write test for x axis of placeShip

---
+++ src/app/gameboard/gameboard.test.js
@@ -0,0 +1,20 @@
+import { GameBoard } from "./gameboard.js";
+import { Ship } from "../ship/ship.js";
+
+describe("Testing placements", () => {
+
+    test("Ship has been placed along x axis", () => {
+        const testShip = new Ship(4, 0);
+        const testBoard = new GameBoard;
+        let isHorizontal = true;
+        let [xCoord, yCoord] = [0, 0];
+
+        testBoard.placeShip(testShip, xCoord, yCoord, isHorizontal);
+        
+        expect(testBoard.board[0][yCoord]).toEqual(testShip);
+        expect(testBoard.board[1][yCoord]).toEqual(testShip);
+        expect(testBoard.board[2][yCoord]).toEqual(testShip);
+        expect(testBoard.board[3][yCoord]).toEqual(testShip);
+    })
+
+})
