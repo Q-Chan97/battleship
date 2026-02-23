cea948b5049ad408cb1271783df500a7d6a07d2b
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-23 15:39:52 -0600

    feat: Create ship types object

---
+++ src/app/ship/shipTypes.js
@@ -0,0 +1,17 @@
+export const shipTypes = {
+    carrier: {
+        length: 5
+    },
+    battleship: {
+        length: 4
+    },
+    destroyer: {
+        length: 3
+    },
+    submarine: {
+        length: 2
+    },
+    patrol: {
+        length: 2
+    }
+}
