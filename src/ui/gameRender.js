3508e66e8492268dfb05f02b65b46a76725c9573
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-23 15:01:10 -0600

    feat: Implement game board rendering

---
+++ src/ui/gameRender.js
@@ -0,0 +1,54 @@
+export function renderAllBoards(player, computer) {
+    renderSingleBoard(player.gameBoard, player.name, player.type);
+
+    renderSingleBoard(computer.gameBoard, computer.name, computer.type);
+}
+
+function renderSingleBoard(gameBoard, name, type) {
+    const boardDiv = document.createElement("div");
+    boardDiv.classList.add("gameBoard");
+
+    // Generate a 10 x 10 board
+    for (let y = 0; y < gameBoard.rows; y++) {
+        for (let x = 0; x < gameBoard.cols; x++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.textContent = `${x},${y}`; // Temporary, just to check if coordinates are aligned
+
+            // Assign x and y coords to cell's dataset
+            cell.dataset.x = x;
+            cell.dataset.y = y;
+
+            boardDiv.appendChild(cell);
+        }
+    }
+
+    // Append to DOM based on player type
+    if (type === "player") {
+        boardDiv.id = "player-board";
+
+        const playerBoardContainer = document.querySelector("#player-gameBoard-container");
+        playerBoardContainer.innerHTML = "";
+
+        const playerName = document.createElement("h3");
+        playerName.classList.add("board-title");
+        playerName.textContent = `Commander ${name}'s Board`;
+
+        playerBoardContainer.appendChild(playerName);
+        playerBoardContainer.appendChild(boardDiv);
+    }
+
+    else if (type === "computer") {
+        boardDiv.id = "computer-board";
+
+        const computerBoardContainer = document.querySelector("#computer-gameBoard-container");
+        computerBoardContainer.innerHTML = "";
+
+        const computerName = document.createElement("h3");
+        computerName.classList.add("board-title");
+        computerName.textContent = `Enemy Computer's Board`;
+
+        computerBoardContainer.appendChild(computerName);
+        computerBoardContainer.appendChild(boardDiv);
+    }
+}
