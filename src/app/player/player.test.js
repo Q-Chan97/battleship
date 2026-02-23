b5e148c93f640386905a685c6ac0281e6b9389b3
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-23 10:30:06 -0600

    feat: Write attack tests for player class

--- src/app/player/player.test.js
+++ src/app/player/player.test.js
@@ -1,4 +1,5 @@
 import { Player } from "./player.js";
+import { Ship } from "../ship/ship.js";
 
 describe("Player class tests", () => {
     let realPlayer;
@@ -22,5 +23,33 @@ describe("Player class tests", () => {
     test("Players are unique", () => {
         expect(realPlayer).not.toBe(aiPlayer);
         expect(realPlayer.gameBoard).not.toBe(aiPlayer.gameBoard);
+    })
+})
+
+describe("Player attack tests", () => {
+    let realPlayer;
+    let aiPlayer;
+
+    beforeEach(() => {
+        realPlayer = new Player("player1", "real");
+        aiPlayer = new Player("computer", "ai");
+    })
+
+    test("Player can attack enemy board", () => {
+        let ship = new Ship(3, 0);
+        let enemyBoard = aiPlayer.gameBoard;
+        enemyBoard.placeShip(ship, 0, 0, true);
+        realPlayer.attack(enemyBoard, 0, 0);
+        
+        expect(ship.hits).toBe(1);
+    })
+
+    test("Ai player can attack real player board", () => {
+        let ship = new Ship(3, 0);
+        let enemyBoard = realPlayer.gameBoard;
+        enemyBoard.placeShip(ship, 0, 0, true);
+        aiPlayer.attack(enemyBoard, 0, 0);
+
+        expect(ship.hits).toBe(1);
     })
 })
