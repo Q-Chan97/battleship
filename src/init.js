c0aa89ded90c5a6ad1365b2fc2d36691f6e82e57
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-23 16:07:51 -0600

    update: Remove testing from init

--- src/init.js
+++ src/init.js
@@ -1,20 +1,6 @@
 // Initialize JS
-
 import { renderAllBoards } from "./ui/gameRender";
-import { Player } from "./app/player/player";
-import { Ship } from "./app/ship/ship.js";
-
-// Testing
-
-let realPlayer = new Player("Joe", "player");
-let aiPlayer = new Player("AI", "computer")
-
-let battleship = new Ship("battleship", 4);
-let carrier = new Ship("carrier", 5);
 
 export function init() {
-    realPlayer.gameBoard.placeShip(battleship, 0, 0, true);
-    aiPlayer.gameBoard.placeShip(battleship, 0, 0, true);
-    realPlayer.gameBoard.placeShip(carrier, 0, 4, false);
     renderAllBoards(realPlayer, aiPlayer);
 }
