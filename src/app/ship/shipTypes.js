03bc1b8578899d2e49f0482527ee533ee2c7d36b
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-24 14:59:23 -0600

    fix: Add type to shipTypes

--- src/app/ship/shipTypes.js
+++ src/app/ship/shipTypes.js
@@ -1,17 +1,22 @@
 export const shipTypes = {
     carrier: {
+        type: "Carrier",
         length: 5
     },
     battleship: {
+        type: "Battleship",
         length: 4
     },
     destroyer: {
+        type: "Destroyer",
         length: 3
     },
     submarine: {
+        type: "Submarine",
         length: 2
     },
     patrol: {
+        type: "Patrol Boat",
         length: 2
     }
 }
