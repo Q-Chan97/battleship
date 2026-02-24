7606343e7449aa4a38e58322bdeb638c9135abd5
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-24 11:55:28 -0600

    feat: Implement getPlayerName async function

---
+++ src/ui/uiEvents.js
@@ -0,0 +1,22 @@
+// Event listeners
+
+// Player name dialog
+
+export async function getPlayerName() {
+    return new Promise((resolve) => {
+        const submitButton = document.getElementById("player-name-submit-button");
+        const playerNameInput = document.getElementById("player-name-input");
+        const playerNameDialog = document.getElementById("player-name-dialog");
+
+        playerNameDialog.showModal();
+
+        submitButton.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            const name = playerNameInput.value.trim();
+            playerNameDialog.close();
+
+            resolve(name);
+        }, { once: true }); // Stops event from being called multiple times
+    });
+};
