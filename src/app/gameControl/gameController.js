1e93259808792ee2d24a3f99036779c951d2bab2
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-24 15:19:53 -0600

    feat: Implement createPlayerFleet in setupGame

--- src/app/gameControl/gameController.js
+++ src/app/gameControl/gameController.js
@@ -1,4 +1,6 @@
 import { renderAllBoards } from "../../ui/gameRender.js";
+import { displayMessage } from "../../ui/uiEvents.js";
+import { createPlayerFleet } from "../../ui/dragDropShips.js";
 
 export class GameController {
     constructor(player1, player2) {
@@ -16,7 +18,50 @@ export class GameController {
         this.winner = null; // Keep track of win state
     }
 
-    startGame() {
-        renderAllBoards(this.player1, this.player2)
+    setupGame() {
+        renderAllBoards(this.player1, this.player2);
+
+        displayMessage("Place your ships, Commander!");
+
+        createPlayerFleet();
+        
+        // while there are still draggable ships, wait for player to finish placement
+
+        // Have UI call playGame
+
+    }
+
+    playGame() {
+        // Update game state
+        this.isPlanning = false;
+        this.inProgress = true;
+
+        // randomly place ai ships (ai logic js file)
+
+        // take turn player OR ai? check
+
+        // check winner
+
+        // If no winner, switch turn and keep playing : takeTurn => checkWinner => switchTurn
+    }
+
+    takeTurn() {
+        // If current player is player, do player stuff (from player logic js file?);
+
+        // If current player is computer, do computer stuff from ai logic js file
+    }
+
+
+    switchTurn() {
+        // Swap current and other player roles
+    }
+
+    checkWinner() {
+        // If all ships are sunk (allShipsSunk from gameBoard), declare winner for current player
+    }
+
+    gameReset() {
+        // Clear board
+        // Start new game (with same player name?)
     }
 }
