ce847121ecc540a2ec3d34bb2ecbbf39a543d3f9
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-26 14:34:16 -0600

    feat: Write handlePlayerFire and checkSunkShip

---
+++ src/app/player/playerLogic.js
@@ -0,0 +1,49 @@
+// Player actions
+import { displayMessage } from "../../ui/uiEvents";
+
+// Player firing
+export function handlePlayerFire(controller, e) {
+
+    const x = parseInt(e.currentTarget.dataset.x);
+    const y = parseInt(e.currentTarget.dataset.y);
+
+    try {
+        const result = controller.otherPlayer.gameBoard.receivedAttack(x, y);
+
+        if (result === "Hit") {
+            e.currentTarget.classList.add("hit");
+
+            // Check for ship sinking
+            const sunkShip = checkSunkShip(controller.otherPlayer, x, y);
+
+            if (sunkShip) {
+                displayMessage(`Excellent work Commander, we sunk the enemy ${sunkShip.type}!`)
+            } else displayMessage("Great shot Commander, the enemy was hit!");
+        }
+
+        if (result === "Miss") {
+            e.currentTarget.classList.add("miss");
+            displayMessage("Our shot missed, Commander.")
+        }
+
+        // If game is over, return
+        if (controller.checkWinner()) return;
+
+        // Switch to AI
+        controller.switchTurn();
+    }
+
+    catch (error) {
+        alert(error);
+    }
+}
+
+// Finds ship type once sunk
+function checkSunkShip(computer, x, y) {
+    const target = computer.gameBoard.board[x][y];
+
+    if (target && target.isSunk()) {
+        return target;
+    }
+    return null;
+}
