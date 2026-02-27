17de32b17d66bafa0cbf9fda60dd7e33161a65d5
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-02-27 08:45:45 -0600

    feat: Implement new game button in init

--- src/init.js
+++ src/init.js
@@ -2,6 +2,7 @@
 import { GameController } from "./app/gameControl/gameController";
 import { Player } from "./app/player/player";
 import { getPlayerName } from "./ui/uiEvents";
+import { setupResetButton } from "./ui/uiEvents";
 
 export async function init() {
     const playerName = await getPlayerName(); // Wait for player name before launching game
@@ -11,4 +12,5 @@ export async function init() {
 
     const newGame = new GameController(realPlayer, computerPlayer);
     newGame.setupGame();
+    setupResetButton(newGame); // Pass controller instance to button setup
 }
